34bf03be17c76af2b3a5a85566a203bfae09faad
Fernando Balmaceda <phantom2024@gmail.com>
2020-01-13 12:52:29 -0300

    implemented logic to make the ball move into the box the user clicks on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+interface Box {
+  hasCircle: boolean;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'spark-test';
+
+  boxList: Box[] = [];
+  circlePosition: number;
+  lastCirclePosition: number;
+
+  constructor() {
+    
+    this.lastCirclePosition = 0;
+    this.boxList = [
+      {
+        hasCircle: true
+      },
+      {
+        hasCircle: false
+      },
+      {
+        hasCircle: false
+      },
+      {
+        hasCircle: false
+      }
+    ];
+  }
+
+  selectBox(index: number) {
+    // search last circle position
+    this.lastCirclePosition = this.getLastCirclePosition();
+
+    // remove the circle in the latest position
+    this.boxList[this.lastCirclePosition].hasCircle = false;
+
+    // set the new circle position
+    this.boxList[index].hasCircle = true;
+  }
+
+  getLastCirclePosition() {
+    for (const [i, box] of this.boxList.entries()) {
+      if (box.hasCircle) return i;
+    }
+    return 0;
+  }
 }
